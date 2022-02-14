3943d3a877c3be53bd9dbb71a643824fd50e2459
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-14 14:07:00 +0530

    create component and add simple style

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  // styleUrls: ['./server.component.css']
+  styles: [`h3 {
+    color: aqua
+  }`]
+})
+export class ServerComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
